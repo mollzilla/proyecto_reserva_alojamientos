b98a4ef22a5cfe400410021bec646731d44dcd90
mollzilla <50122107+mollzilla@users.noreply.github.com>
2020-11-08 21:03:03 -0300

    view images

--- src/HotelContainer.js
+++ src/HotelContainer.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./styles.css";
-import { hotelsData, today } from "../public/data";
+import { hotelsData, today } from "./data";
 import Header from "./Header";
 import HotelCard from "./HotelCard";
 import EmptySearch from "./EmptySearch";
